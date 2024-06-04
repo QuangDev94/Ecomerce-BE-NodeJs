8a0b0922114d9c0e4be8c480ce14cadb7cd34f1c
QuangDev <ng.quang.coder.new@gmail.com>
2024-06-04 18:25:27 +0700

    Design model for database and connect mongodb

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+dotenv.config();
+
 app.get("/", (req, res) => {
   res.send("Hello World 123123");
 });
+
+mongoose
+  .connect(
+    `mongodb+srv://ngquangcodernew:${process.env.MONGO_DB}@cluster0.kqjumls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    console.log("Success");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log("Server is running in port: ", port);
---
+++ src/models/UserModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: Number, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+    access_token: { type: String, required: true },
+    refresh_token: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
